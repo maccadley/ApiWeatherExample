6fba07f9ea9567e1f3ea235f24e331c5af64d237
maccadley <maximmasov@gmail.com>
2023-09-11 23:35:10 +0300

    Initial commit second

---
+++ ApiSolution/src/main/java/com/testCase/ApiSolution/ApiSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.testCase.ApiSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSolutionApplication.class, args);
+	}
+
+}
